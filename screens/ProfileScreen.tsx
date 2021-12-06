64e8b15445f0894c32803dcf4e99b017518fc288
Adam Wolfman <adamwolfman@Adams-MBP-3.domain>
2021-12-06 15:54:54 -0700

    Add profile table ui

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,23 +1,34 @@
 import * as React from 'react';
-import { StyleSheet, ScrollView } from 'react-native';
+import { StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
 import { Text, View } from '../components/Themed';
+import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
 
 
 export default function ProfileScreen(props: Object) {
-  
-  
+  const tableHead = ['Profile Key', 'Profile Value'];
+  const tableData = [
+    ['First Name', props.profile.first_name],
+    ['Last Name', props.profile.last_name],
+    ['Email', props.profile.email],
+    ['Profile ID', props.profile.id],
+    ['Connection Type', props.profile.connection_type],
+    ['Connection ID',props.profile.connection_id],
+    ['Raw Attributes', <TouchableOpacity><Text>View Raw Attributes</Text></TouchableOpacity>],
+  ]
+
   return (
     <View style={styles.container}>
-        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-        <ScrollView>
-          <Text>{props.profile.first_name}</Text>
-          <Text>{props.profile.last_name}</Text>
-          <Text>{props.profile.email}</Text>
-          <Text>{props.profile.id}</Text>
-          <Text>{props.profile.connection_type}</Text>
-          <Text>{props.profile.connection_id}</Text>
+        <View style={[styles.tableContainer]}>
+          <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
+            <Row data={tableHead} style={styles.head} textStyle={styles.text}/>
+            <Rows data={tableData} textStyle={styles.text}/>
+          </Table>
+        </View>
+
+        {/* <ScrollView>
           <Text>{JSON.stringify(props.profile.raw_attributes)}</Text>
-        </ScrollView>
+        </ScrollView> */}
+
     </View>
   );
 }
@@ -33,9 +44,36 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   separator: {
-    marginVertical: 30,
+    marginVertical: 15,
     height: 1,
     width: '80%',
   },
-
+  button: {
+    borderColor: '#6363F1',
+    borderStyle: "solid",
+    borderWidth: 1,
+    padding: 5,
+    borderRadius: 5,
+    position: 'relative',
+    bottom: 300,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#6363F1',
+    textAlign: 'center',
+  },
+  head: { 
+    height: 40, 
+    backgroundColor: '#f1f8ff'
+  },
+  text: { 
+    margin: 6
+  },
+  tableContainer: { 
+    flex: 2,
+    padding: 16,
+    position: 'relative',
+    bottom: 15, 
+    width: '70%' 
+  },
 });
