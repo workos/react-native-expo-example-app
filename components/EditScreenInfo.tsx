a7f9b01daf66ad9ae0956cf4f8095e1e1d176b0a
Logan Gingerich <logangingerich@yahoo.com>
2021-12-06 09:07:57 -0700

    Add environment variable setup

--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -1,5 +1,3 @@
-
-import * as WebBrowser from 'expo-web-browser';
 import React, { useState } from 'react';
 import { StyleSheet, Button, TouchableOpacity, TextInput } from 'react-native';
 import { withSafeAreaInsets } from 'react-native-safe-area-context';
@@ -8,16 +6,12 @@ import Colors from '../constants/Colors';
 import { MonoText } from './StyledText';
 import {Text, View } from './Themed';
 import * as AuthSession from 'expo-auth-session';
-import * as Linking from 'expo-linking';
+import {WORKOS_API_KEY, WORKOS_CLIENT_ID} from '@env'
 
 
 
 export default function EditScreenInfo({ path }: { path: string }) {
-
   const [profile, setProfile] = useState<String | null>();
-  
-
-  console.log(process.env.CONFIGVAR1);
 
   const reset = () => {
     setProfile(null)
@@ -25,21 +19,21 @@ export default function EditScreenInfo({ path }: { path: string }) {
 
   async function getAuthURL(): Promise<any> {
     let redirect = AuthSession.makeRedirectUri().toString();
-    let connection_id = 'conn_01FNYP9FHYPEYN268C3D0RJJ7Z';
-    let client_id = 'client_01FA12C7QV793K318T2G1V3E7X';
+    let connection_id = 'conn_01FFTPF9NCTBVWX4DHJ3KEJQRZ';
+    let client_id = process.env.WORKOS_CLIENT_ID;
     
     let url = `https://api.workos.com/sso/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirect}&state=&connection=${connection_id}`;
     let result = await AuthSession.startAsync({authUrl: url, returnUrl: redirect});
     let code = JSON.parse(JSON.stringify(result)).params.code;
-    global.code = code;
-    getProfile(client_id);
+    getProfile(client_id, code);
   }
 
-  async function getProfile(client_id: String): Promise<any> {
-    const apiKey = 'sk_test_a2V5XzAxRkExMkM3TTNSTldFNUNKSEFNUUVZQ1pTLDJtb3drUExOTk9vT3dDc1NDRTZnRUVVQ28'
+  async function getProfile(client_id: String, code: String): Promise<any> {
+    const apiKey = process.env.WORKOS_API_KEY
+    
     axios({
       method: 'post',
-      url: `https://api.workos.com/sso/token?client_id=${client_id}&client_secret=${apiKey}&grant_type=authorization_code&code=${global.code}`
+      url: `https://api.workos.com/sso/token?client_id=${client_id}&client_secret=${apiKey}&grant_type=authorization_code&code=${code}`
     }).then((response) => {
       console.log(response.data);
       setProfile(JSON.stringify(response.data));
